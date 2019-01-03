14bc6864ff34a67d9cdec3e4706b9b9ba3799e86
alex <alexliu12@gmail.com>
2019-01-02 20:08:27 -0800

    refactored to remove carousel for now, max 4 swatches

--- client/src/components/SimilarListItem.jsx
+++ client/src/components/SimilarListItem.jsx
@@ -1,23 +1,9 @@
 /* eslint-disable import/extensions */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
+
 import React from 'react';
-import { TransitionGroup } from 'react-transition-group';
-import $ from 'jquery';
 import ListItemColors from './ListItemColors.jsx';
-
-
-// eslint-disable-next-line arrow-body-style
-// const SimilarListItem = (props) => {
-//   return (
-//     <div className="SimilarListItem">
-//       {console.log('props similar item', props)}
-//       <img src={props.item.photo_url} alt="new" />
-//       <p>Item description</p>
-//       <p>$50.00</p>
-//     </div>
-//   );
-// };
 
 class SimilarListItem extends React.Component {
   constructor(props) {
@@ -60,9 +46,9 @@ class SimilarListItem extends React.Component {
             <img className="SimilarHoverImage" src={this.props.item[0].colors[0].photos[1].photo_url} alt="" />
           </div>
           <div className="ColorSwatchesContainer">
-            <a className="arrow-left arrow"><img className="ArrowPic" src="https://s3-us-west-1.amazonaws.com/lubucket3003/leftarrow.png" alt="" /></a>
-            <a className="arrow-right arrow"><img className="RightArrowPic" src="https://s3-us-west-1.amazonaws.com/lubucket3003/leftarrow.png" alt="" /></a>
-            <ListItemColors colors={this.props.item[0].colors} key={this.props.item[0].colors.id} />
+            {/* <a className="arrow-left arrow"><img className="ArrowPic" src="https://s3-us-west-1.amazonaws.com/lubucket3003/leftarrow.png" alt="" /></a>
+            <a className="arrow-right arrow"><img className="RightArrowPic" src="https://s3-us-west-1.amazonaws.com/lubucket3003/leftarrow.png" alt="" /></a> */}
+            <ListItemColors colors={this.props.item[0].colors.slice(0, 4)} key={this.props.item[0].colors.id} />
           </div>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" />
           <script src="../../dist/clickscript.js" />
@@ -71,7 +57,6 @@ class SimilarListItem extends React.Component {
         // this.state.isHovering && <ListItemColors colors={this.props.item[0].colors}
         // key={this.props.item[0].colors.id} />
         }
-        {/* </TransitionGroup> */}
         <div id="similarItemName">{this.props.item[0].name}</div>
         <span id="similarItemPrice">{this.props.item[0].price}</span>
         <span id="similarItemCurrency"> USD</span>
